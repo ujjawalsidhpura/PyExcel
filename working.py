4e9e33adf7fd8c42908ec3f42fd1f122fd2e1ffb
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-02 14:12:58 -0500

    loop and save

--- working.py
+++ working.py
@@ -45,3 +45,12 @@ ws.append(headings)
 for person in data:
     grades = list(data[person].values())
     ws.append([person] + grades)
+
+for col in range(2, len(data['Joe']) + 2):
+    char = get_column_letter(col)
+    ws[char + "7"] = f"=SUM({char + '2'}:{char + '6'})/{len(data)}"
+
+for col in range(1, 6):
+    ws[get_column_letter(col) + '1'].font = Font(bold=True, color="0099CCFF")
+
+wb.save("NewGrades.xlsx")
